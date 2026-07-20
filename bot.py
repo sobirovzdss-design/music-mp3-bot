84a049a248552b0d09a368e9db0995f296cbf231
sobirovzdss-design <sobirov.zdss@gmail.com>
2026-07-20 12:36:22 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 TOKEN = "8764326925:AAGf5SIHFfi9w4-OqMGEpcd6BPdYjMhIVPo"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🎵 Assalomu alaykum!\n\nBot ishga tushdi!")
+    await update.message.reply_text("🎵 Assalomu alaykum!\n\nBot muvaffaqiyatli ishga tushdi!")
 
 app = Application.builder().token(TOKEN).build()
 
